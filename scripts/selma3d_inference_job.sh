47deb4da7555799bb692e64d56ad022be80b136a
Adina <ads4015@med.cornell.edu>
2025-07-18 19:07:50 -0400

    Refactor inference

--- scripts/selma3d_inference_job.sh
+++ scripts/selma3d_inference_job.sh
@@ -8,20 +8,23 @@
 #SBATCH --mem=32G
 #SBATCH --time=04:00:00
 
+
+# indicate starting
+echo "Starting the inference script..."
+
+
 # activate conda environment
 module load anaconda3/2022.10-34zllqw
 source activate monai-env1
 
-echo "Starting the inference script..."
-
-
 # Run inference on folder
 python /home/ads4015/ssl_project/src/selma3d_inference.py \
-  --input_path /midtier/paetzollab/scratch/ads4015/data_selma3d/unlabeled_val \
+  --input_path /midtier/paetzollab/scratch/ads4015/data_selma3d/selma3d_unlabeled_val \
   --checkpoint /home/ads4015/ssl_project/lsm_finetune/vtmxkihh/checkpoints/best.ckpt \
-  --output_dir /midtier/paetzollab/scratch/ads4015/data_selma3d/unlabeled_val_preds
+  --output_dir /midtier/paetzollab/scratch/ads4015/data_selma3d/selma3d_unlabeled_val_preds
 
 
+# indicate completion
 echo "Inference script completed successfully."
 
 
