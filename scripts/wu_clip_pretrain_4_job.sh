3be00b2b9f41ed8bac8a7fa456b84c4fe633fac8
Adina <ads4015@med.cornell.edu>
2025-08-15 11:19:27 -0400

    Add pretraining configs and scripts for 1,2,4 gpus

---
+++ scripts/wu_clip_pretrain_4_job.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#SBATCH --job-name=clip_pretrain
+#SBATCH --output=logs/clip_pretrain_%j.out
+#SBATCH --error=logs/clip_pretrain_%j.err
+#SBATCH --time=12:00:00
+#SBATCH --partition=minilab-gpu
+#SBATCH --gres=gpu:h100:4 # select number of gpus ***
+#SBATCH --mem=180G
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4 # 1 task per gpu ***
+
+
+# wu_clip_pretrain_4_job.sh - Script for Wu data pretraining
+
+# indicate starting
+echo "Starting LSM pretraining..."
+
+
+
+# activate conda environment
+module load anaconda3/2022.10-34zllqw
+source activate monai-env1
+
+# wandb login
+export WANDB_API_KEY=9778703390de02a48bdea1415c3c36c3bae408c0
+wandb login $WANDB_API_KEY
+
+export TOKENIZERS_PARALLELISM=false
+export NCCL_P2P_DISABLE=0 # when set to 0, allows NCCL to use P2P communication for better performance (instead of using CPU for communication)
+export NCCL_IB_DISABLE=0 # when set to 0, allows NCCL to use InfiniBand for better performance (instead of using TCP/IP for communication)
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1 # when set to 1, allows NCCL to handle errors asynchronously (can improve performance in some cases)
+
+# set path to config file
+CONFIG_PATH="/home/ads4015/ssl_project/configs/wu_clip_pretrain_4_config.yaml"
+
+# clock job start time
+export START_EPOCH="$(date +%s)"
+echo "[INFO] Job runtime timer started at $(date -d @${START_EPOCH} '+%Y-%m-%d %H:%M:%S')"
+
+# allocator
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+# run script
+srun --label python /home/ads4015/ssl_project/src/wu_clip_pretrain.py --config $CONFIG_PATH
+
+
+
+# indicate completion
+echo 'LSM pretraining complete'
+
+
+
+
+
+
+
+
+
+
+
+
+
